c0dd17edc77832ab28409260baf1d19fe785a23a
antonstrand <jag@antonstrand.se>
2020-09-24 19:43:40 +0200

    leader key

--- keyboards/dz60/keymaps/antonstrand/keymap.c
+++ keyboards/dz60/keymaps/antonstrand/keymap.c
@@ -1,48 +1,24 @@
 #include QMK_KEYBOARD_H
 #include "keymap_swedish.h"
+#include "sendstring_swedish.h"
 #define ______ KC_TRNS
 
 /** Custom Keys */
-enum custom_keycodes {
-    CK_CUT   = LGUI(KC_X),
-    CK_COPY  = LGUI(KC_C),
-    CK_PASTE = LGUI(KC_V),
-    CK_UNDO  = LGUI(KC_Z),
-    CK_FIND  = LGUI(KC_F),
-};
 
+enum custom_keycodes { CK_CUT = LGUI(KC_X), CK_COPY = LGUI(KC_C), CK_PASTE = LGUI(KC_V), CK_UNDO = LGUI(KC_Z), CK_FIND = LGUI(KC_F), CK_LCBR = LSFT(LALT(SE_8)), CK_RCBR = LSFT(LALT(SE_9)), PIPE = SAFE_RANGE, COMPOSE, SKINNY_ARROW, SELECT_APP, SELECT_WORD, CK_DOWN_5, CK_UP_5 };
 /** LAYERS */
 #define BASE 0
-#define SWE 1
-#define ARROW 2
-#define MOUSE 3
+#define ARROW 1
+#define MOUSE 2
+#define SYMBOL 3
 #define RGB 4
+#define RAPID 5
+
+enum { TD_RCBR = 0, TD_RBRC = 1 };
+
+qk_tap_dance_action_t tap_dance_actions[] = {[TD_RCBR] = ACTION_TAP_DANCE_DOUBLE(CK_LCBR, CK_RCBR), [TD_RBRC] = ACTION_TAP_DANCE_DOUBLE(SE_LBRC, SE_RBRC)};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    /* Base Layer
-     * ,-----------------------------------------------------------------------------------------.
-     * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
-     * |-----------------------------------------------------------------------------------------+
-     * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
-     * |-----------------------------------------------------------------------------------------+
-     * | LAY1    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
-     * |-----------------------------------------------------------------------------------------+
-     * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | SWE |
-     * |-----------------------------------------------------------------------------------------+
-     * | Ctrl |  Alt  |  Cmd  | Bkspc/MOUSE | Spc |   ARROW/SPACE   | RAlt | RGB |  L |  D  |  R |
-     * `-----------------------------------------------------------------------------------------'
-     */
-
-    LAYOUT_all(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, SE_MINS, SE_EQL, ______, KC_BSPC,
-
-               KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, SE_LBRC, SE_RBRC, SE_BSLS,
-
-               LT(MOUSE, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-
-               KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, TG(SWE),
-
-               KC_LCTL, KC_LALT, KC_LGUI, LT(MOUSE, KC_BSPC), KC_SPC, LT(ARROW, KC_SPC), KC_RALT, TG(RGB), KC_LEFT, KC_DOWN, KC_RIGHT),
 
     /* Swedish Layer
      * ,-----------------------------------------------------------------------------------------.
@@ -50,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------+
      * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  Å  |  ¨  |    '   |
      * |-----------------------------------------------------------------------------------------+
-     * | LAY1    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Ö  |  Ä  |    Enter    |
+     * | SYMBOL  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Ö  |  Ä  |    Enter    |
      * |-----------------------------------------------------------------------------------------+
-     * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  -  | RSh |  U | Base |
+     * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  -  | RSh |  U | Del  |
      * |-----------------------------------------------------------------------------------------+
-     * | Ctrl |  Alt  |  Cmd  | Bkspc/MOUSE | ARROW/Spc |   SPACE   | RAlt | RGB |  L |  D  |  R |
+     * | Ctrl |  Alt  |  Cmd  | Bkspc/MOUSE | LEAD |   ARROW/SPACE   | RAlt | RGB |  L |  D  |  R |
      * `-----------------------------------------------------------------------------------------'
      */
 
@@ -62,39 +38,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 
-               LT(MOUSE, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, SE_ODIA, SE_ADIA, KC_ENT,
+               OSL(SYMBOL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, SE_ODIA, SE_ADIA, KC_ENT,
 
-               KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, ______,
+               OSM(MOD_LSFT), ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
 
-               ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______),
+               KC_LCTL, KC_LALT, KC_LGUI, LT(MOUSE, KC_BSPC), KC_LEAD, LT(ARROW, KC_SPC), KC_RALT, TG(RGB), KC_LEFT, KC_DOWN, KC_RIGHT),
 
     /* Arrow Layer
      * ,-----------------------------------------------------------------------------------------.
-     * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   DEL     |
+     * | Esc |     |     |     |     |     |     |     |     |     |     |     |     |   DEL     |
      * |-----------------------------------------------------------------------------------------+
-     * |       |     |     | Enter | Del | Find |   | Undo | Cut | Copy | Paste |    |    |      |
+     * |       |     | Slt word | Enter | Del | Find |   | Undo | Cut | Copy | Paste |    |    |      |
      * |-----------------------------------------------------------------------------------------+
-     * |         |  LSh | LCtrl | LAlt | LCmd |   |   | Left | Down |  Up | Right |     |        |
+     * |         |  LSh | LCtrl | LAlt | LCmd | Slt App |  | Left | Down |  Up | Right |  |      |
      * |-----------------------------------------------------------------------------------------+
-     * |           | Undo | Cut | Copy | Paste | Esc | Enter |   |   |    |    |     |VOL+| MUTE |
+     * |           | Undo | Cut | Copy | RAPID |    | Esc | Enter |   |    |    |    |VOL+| MUTE |
      * |-----------------------------------------------------------------------------------------+
      * |      |       |       |                                   |     |     | BRI- |VOL-| BRI+ |
      * `-----------------------------------------------------------------------------------------'
      */
 
-    LAYOUT_all(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ______, KC_DEL,
+    LAYOUT_all(______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_DEL,
 
-               ______, ______, KC_ESC, KC_ENT, KC_DEL, CK_FIND, ______, CK_UNDO, CK_CUT, CK_COPY, CK_PASTE, ______, ______, ______,
+               ______, ______, SELECT_WORD, KC_ENT, KC_DEL, CK_FIND, ______, CK_UNDO, CK_CUT, CK_COPY, CK_PASTE, ______, ______, ______,
 
-               ______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, ______, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ______, ______,
+               ______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, SELECT_APP, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ______, ______,
 
-               ______, ______, CK_UNDO, CK_CUT, CK_COPY, CK_PASTE, ______, KC_ESC, KC_ENT, ______, ______, ______, ______, KC_VOLU, KC_MUTE,
+               ______, ______, CK_UNDO, CK_CUT, CK_COPY, MO(RAPID), ______, KC_ESC, KC_ENT, ______, ______, ______, ______, KC_VOLU, KC_MUTE,
 
                ______, ______, ______, ______, ______, ______, ______, ______, KC_BRID, KC_VOLD, KC_BRIU),
 
     /* Mouse Layer
      * ,-----------------------------------------------------------------------------------------.
-     * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   DEL     |
+     * | Esc |     |     |     |     |     |     |     |     |     |     |     |     |           |
      * |-----------------------------------------------------------------------------------------+
      * |       |     | Spd 1 | Spd 2 | Spd 3 |     |     |     |     |     |     |      |        |
      * |-----------------------------------------------------------------------------------------+
@@ -115,6 +91,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                ______, ______, ______, ______, ______, ______, ______, ______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, ______, ______,
 
                ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ),
+
+    /* Symbol Layer
+     * ,-----------------------------------------------------------------------------------------.
+     * | Esc |    |     |  #  |  $   |  %  |     |     |     |     |     |     |     |           |
+     * |-----------------------------------------------------------------------------------------+
+     * |       |  "  |  '  |  (|)  | & |  /   |   (   |   )  |      |  -  |  +  |   =  |    ~    |
+     * |-----------------------------------------------------------------------------------------+
+     * |         |  @  |  >>  | -> | (|>) |    |    |  [  |   ]  |   {   |   }  |  *  |          |
+     * |-----------------------------------------------------------------------------------------+
+     * |           |     |    |    |    |    |    |  (\)  |   <   |   >   |     |   :  |    |    |
+     * |-----------------------------------------------------------------------------------------+
+     * |      |       |       |                                      |     |     |     |    |    |
+     * `-----------------------------------------------------------------------------------------'
+     */
+
+    LAYOUT_all(______, ______, ______, SE_HASH, A(SE_4), SE_PERC, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+
+               ______, SE_DQUO, SE_QUOT, A(KC_7), SE_AMPR, SE_SLSH, ______, S(SE_MINS), SE_LPRN, SE_RPRN, SE_MINS, SE_PLUS, SE_EQL, SE_TILD,
+
+               ______, SE_AT, COMPOSE, SKINNY_ARROW, PIPE, ______, ______, KC_GRV, TD(TD_RBRC), TD(TD_RCBR), S(KC_GRV), SE_ASTR, ______,
+
+               ______, ______, ______, ______, ______, SE_BSLS, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+
+               ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______),
 
     /* RGB Layer
      * ,-----------------------------------------------------------------------------------------.
@@ -139,11 +139,110 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
 
                ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______),
+
+    /* Rapid Layer
+     * ,-----------------------------------------------------------------------------------------.
+     * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
+     * |-----------------------------------------------------------------------------------------+
+     * |       |     |      |      |      |      |      |      |     |     |     |      |        |
+     * |-----------------------------------------------------------------------------------------+
+     * |         |     |     |      |     |    |    |     | Down 5 |  Up 5 |     |    |          |
+     * |-----------------------------------------------------------------------------------------+
+     * |           |     |     |    |     |    |     |      |     |      |     |     |     |     |
+     * |-----------------------------------------------------------------------------------------+
+     * |      |       |       |                                      |     |     |     |    |    |
+     * `-----------------------------------------------------------------------------------------'
+     */
+
+    LAYOUT_all(______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+
+               ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+
+               ______, ______, ______, ______, ______, ______, ______, ______, CK_DOWN_5, CK_UP_5, ______, ______, ______,
+
+               ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+
+               ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ),
 };
 
 void matrix_init_user(void) { rgblight_enable(); }
 
+LEADER_EXTERNS();
+
+void mod_type(uint16_t modcode, uint16_t keycode) {
+    register_code16(modcode);
+    tap_code16(keycode);
+    unregister_code16(modcode);
+}
+
+void select_word(void) {
+    register_code16(KC_LALT);
+    tap_code16(KC_RIGHT);
+    tap_code16(KC_LEFT);
+    register_code16(KC_LSFT);
+    tap_code16(KC_RIGHT);
+    unregister_code16(KC_LALT);
+    unregister_code16(KC_LSFT);
+}
+
+void select_line(void) {
+    register_code16(KC_LCMD);
+    tap_code16(KC_RIGHT);
+    tap_code16(KC_LEFT);
+    register_code16(KC_LSFT);
+    tap_code16(KC_RIGHT);
+    unregister_code16(KC_LCMD);
+    unregister_code16(KC_LSFT);
+}
+
+void select_full_line(void) {
+    register_code16(KC_LCMD);
+    tap_code16(KC_RIGHT);
+    tap_code16(KC_LEFT);
+    tap_code16(KC_LEFT);
+    register_code16(KC_LSFT);
+    tap_code16(KC_RIGHT);
+    unregister_code16(KC_LCMD);
+    unregister_code16(KC_LSFT);
+}
+
 void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        SEQ_ONE_KEY(KC_L) { select_line(); }
+        SEQ_ONE_KEY(KC_U) { tap_code16(G(KC_Z)); }
+        SEQ_ONE_KEY(KC_W) { select_word(); }
+        SEQ_TWO_KEYS(KC_L, KC_L) { select_full_line(); }
+        SEQ_ONE_KEY(KC_O) {
+            tap_code16(KC_DOWN);
+            mod_type(KC_LGUI, KC_LEFT);
+            tap_code16(KC_ENT);
+            tap_code16(KC_UP);
+        }
+        SEQ_TWO_KEYS(KC_O, KC_O) {
+            mod_type(KC_LGUI, KC_LEFT);
+            tap_code16(KC_ENT);
+            tap_code16(KC_UP);
+        }
+
+        SEQ_TWO_KEYS(KC_D, KC_D) {
+            select_full_line();
+            tap_code(KC_DEL);
+            tap_code(KC_DEL);
+            register_code16(KC_LALT);
+            tap_code16(KC_RIGHT);
+            unregister_code16(KC_LALT);
+        }
+
+        SEQ_TWO_KEYS(KC_W, KC_Q) {
+            register_code(KC_LGUI);
+            tap_code(KC_S);
+            unregister_code(KC_LGUI);
+        }
+        SEQ_FIVE_KEYS(KC_R, KC_E, KC_S, KC_E, KC_T) { tap_code16(RESET); }
+
+        leading = false;
+        leader_end();
+    }
 #ifdef RGBLIGHT_ENABLE
 
     static uint8_t old_layer = 255;
@@ -154,7 +253,7 @@ void matrix_scan_user(void) {
             case BASE:
                 rgblight_setrgb(0x00, 0x00, 0xFF);
                 break;
-            case SWE:
+            case SYMBOL:
                 rgblight_setrgb(0x00, 0xA0, 0xFF);
                 break;
             case ARROW:
@@ -174,40 +273,59 @@ void matrix_scan_user(void) {
 #endif  // RGBLIGHT_ENABLE
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) { return true; }
+void repeat_tap(uint16_t times, uint16_t keycode) {
+    int i;
+    for (i = 0; i < times; i++) {
+        tap_code16(keycode);
+    }
+}
 
-// [SWE] = LAYOUT(
-//   KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-//   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, SE_ARNG, OSL(ENG), KC_BSLS,
-//   KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, SE_ODIA, SE_ADIA, KC_ENT,
-//   KC_LSFT, SE_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-//   KC_LCTL, KC_LALT, KC_LGUI, LT(MOUSE, KC_BSPC), TT(ARROW), KC_SPC, KC_RALT, TT(MOUSE), KC_LEFT, KC_DOWN, KC_RGHT),
-
-// [ENG] = LAYOUT(
-// 	KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-// 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-// 	KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-// 	KC_LSFT, SE_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-// 	_______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
-// ),
-
-// [ARROW] = LAYOUT(
-//   KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
-//   KC_NO, KC_NO, KC_NO, KC_NO, KC_FIND, KC_NO, KC_COPY, KC_UNDO, KC_NO, KC_NO, KC_PSTE, KC_NO, KC_NO, KC_NO,
-//   KC_NO, KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO,
-//   KC_NO, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BRIU, KC_MUTE,
-//   _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_BRID, KC_VOLU),
-
-// [MOUSE] = LAYOUT(
-//   KC_NO, TG(SWE), TG(ENG), TG(ARROW), TG(MOUSE), TG(RGB), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-//   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_R, KC_NO,
-//   KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_MS_U, KC_NO, KC_NO,
-//   KC_NO, KC_NO, KC_ACL2, KC_ACL1, KC_ACL0, KC_WH_D, KC_WH_U, KC_NO, KC_NO, KC_MS_D, KC_BTN1, KC_MS_U, KC_BTN2,
-//   _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R),
-
-// [RGB] = LAYOUT(
-// 	KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_DEL,
-// 	_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
-// 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-// 	_______, _______, _______, _______, BL_DEC, BL_TOGG, BL_INC, BL_STEP, _______, _______, _______, _______, _______, _______,
-// 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case PIPE:
+            if (record->event.pressed) {
+                // |>
+                tap_code16(A(KC_7));
+                tap_code16(S(KC_GRV));
+            }
+            break;
+        case COMPOSE:
+            if (record->event.pressed) {
+                // >>
+                tap_code16(S(KC_GRV));
+                tap_code16(S(KC_GRV));
+            }
+            break;
+        case SKINNY_ARROW:
+            if (record->event.pressed) {
+                // ->
+                SEND_STRING("-");
+                tap_code16(S(KC_GRV));
+            }
+            break;
+        case SELECT_APP:
+            if (record->event.pressed) {
+                register_code16(KC_LCMD);
+                tap_code16(KC_TAB);
+            } else {
+                unregister_code16(KC_LCMD);
+            }
+            break;
+        case SELECT_WORD:
+            if (record->event.pressed) {
+                select_word();
+            }
+            break;
+        case CK_DOWN_5:
+            if (record->event.pressed) {
+                repeat_tap(5, KC_DOWN);
+            }
+            break;
+        case CK_UP_5:
+            if (record->event.pressed) {
+                repeat_tap(5, KC_UP);
+            }
+            break;
+    }
+    return true;
+};
--- keyboards/dz60/rules.mk
+++ keyboards/dz60/rules.mk
@@ -17,12 +17,13 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = no	# Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable per-key backlight LEDs
 AUDIO_ENABLE = no    # There is no speaker on this PCB
 RGBLIGHT_ENABLE = yes # Enable the RGB underglow LEDs
+LEADER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_abnt2
