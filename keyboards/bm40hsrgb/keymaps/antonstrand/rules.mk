abc2f97aded88135628757955b3a9653399b26d7
antonstrand <jag@antonstrand.se>
2020-12-20 17:43:19 +0100

    Enable dynamic macros

--- keyboards/bm40hsrgb/keymaps/antonstrand/rules.mk
+++ keyboards/bm40hsrgb/keymaps/antonstrand/rules.mk
@@ -9,6 +9,7 @@ BOOTLOADER = atmel-dfu
 #
 COMBO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
