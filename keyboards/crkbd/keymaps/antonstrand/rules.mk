adcb802f1ec9b2988392d45a8aa21dbef3cabc7a
Anton Strand <anton.strand@insurello.se>
2021-01-30 13:58:06 +0100

    Enable sound control

--- keyboards/crkbd/keymaps/antonstrand/rules.mk
+++ keyboards/crkbd/keymaps/antonstrand/rules.mk
@@ -1,5 +1,6 @@
 COMBO_ENABLE = yes
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 WPM_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
